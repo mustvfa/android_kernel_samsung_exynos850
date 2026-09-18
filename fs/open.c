329f561de16fadef1da7f67756cdad81602f63f6
mustvfa <mustvfa@proton.me>
2026-09-18 18:39:53 +0300

    add ReSukiSU support

--- fs/open.c
+++ fs/open.c
@@ -344,6 +344,12 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return ksys_fallocate(fd, mode, offset, len);
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot)) 
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user,
+				int *mode, int *flags);
+#endif
+
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
@@ -446,7 +452,10 @@ out:
 
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
-	return do_faccessat(dfd, filename, mode);
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+#endif	
+    return do_faccessat(dfd, filename, mode);
 }
 
 SYSCALL_DEFINE2(access, const char __user *, filename, int, mode)
